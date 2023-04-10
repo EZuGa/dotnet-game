4e8f68c524e7b542f3f9285eb7322b472a4515c4
EZU <gioc@example.com>
2023-04-10 21:15:43 +0400

    added service

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,24 +10,26 @@ namespace dotnet_game.Controllers
     [Route("api/[controller]")]
     public class CharacterController: ControllerBase
     {
-        private static List<Character> characters = new List<Character>{
-            new Character(),
-            new Character(){Id = 1, Name = "Sam"}
-        };
+        
+        private ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get(){
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<List<Character>> GetSingle(int id){
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
         [HttpPost]
         public ActionResult<List<Character>> AddCharacter(Character character){
-            characters.Add(character);
-            return characters;
+            return Ok(_characterService.AddCharacter(character));
         }
     }
 }
